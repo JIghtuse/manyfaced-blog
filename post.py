432062a0a38606e117252668b07bf513f1972f9f
Boris Egorov <egorov@linux.com>
2016-11-05 00:44:06 +0700

    post: Display differently on permalink and main pages

--- post.py
+++ post.py
@@ -10,7 +10,8 @@ class Post(ndb.Model):
     creation_date = ndb.DateTimeProperty(auto_now_add=True)
     author = ndb.KeyProperty(kind=User, required=True)
 
-    def render(self):
+    def render(self, permalink=False):
         author = User.get_by_id(self.author.id())
         return BlogHandler.render_template("post.html", post=self,
-                                           author=author.name)
+                                           author=author.name,
+                                           permalink=permalink)
